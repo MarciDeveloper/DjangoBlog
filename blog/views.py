78e9fbc165b92a7ef2489384612fe4f33b0d0fd5
Marcello Caccamo <londonliving89@gmail.com>
2022-04-24 11:23:02 +1000

    django blog first upload

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Post
+from django.views import generic
+
+# Create your views here.
+
+
+class BlogView(generic.DetailView):
+    model = Post
+    template_name = 'blog.html'
+
+class HomeView(generic.TemplateView):
+    template_name = 'index.html'
